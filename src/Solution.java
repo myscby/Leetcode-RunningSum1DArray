91cbcce83c46eda3e36eae35a9131f194faad17b
myscby <111803960+myscby@users.noreply.github.com>
2022-08-26 21:19:30 -0600

    Add files via upload

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] num = new int[5];
+        num = new int[]{1,3,5,7,9};
+        int total = 0;
+        int[] arr = new int[5];
+
+        for (int i = 0; i < num.length; i++) {
+            total += num[i];
+            arr[i] = total;
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+
+}
